8217a7dd18eaaebdc50c80f7acb04f3f7524a82e
tiroa <95303626+tiroa@users.noreply.github.com>
2021-12-06 15:52:13 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
 namespace fibo {
     /**
-     * Siguiente Fibo
+     * Calcula Fibonacci del número dado
      */
     //% blockId=fibonumber
-    //% block="Mostrar $n"
+    //% block="Mostrar Fibonacci de $n"
     export function fibonaccii(n : number) : void{
-        basic.showNumber(n);
+        if(n==0)
+        basic.showNumber(n)
+        else if(n==1)
+        basic.showNumber(n)
+        else{
+            let f0 : number = 0;
+            let f1 : number = 1;
+            let fn : number = 1;
+            for(let i = 0; i < n;i++){
+                f0 = f1;
+                f1 = fn;
+                fn = f1 + f0;
+            }
+            basic.showNumber(fn);
+        }
     }
 }
